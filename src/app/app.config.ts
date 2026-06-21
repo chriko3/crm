a451f420a351dfb86c58c0784d9534b3fe6ec554
chriko3 <christoph.kohout03@gmail.com>
2026-06-21 14:01:21 +0200

    refactor: remove Firebase integration and user service

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,15 +1,11 @@
 import { ApplicationConfig, provideZonelessChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
-import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
-import { provideDatabase, getDatabase } from '@angular/fire/database';
-import { environment } from '../environments/environment';
+
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZonelessChangeDetection(),
     provideRouter(routes),
-    provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
-    provideDatabase(() => getDatabase()),
   ],
 };
